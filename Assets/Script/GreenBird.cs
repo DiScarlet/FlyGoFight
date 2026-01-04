70288e1c435f4cd8ce111ef91621519c896e725e
DiScarlet <antoniukdin@gmail.com>
2026-01-04 18:06:16 +0100

    Added throw

--- Assets/Script/GreenBird.cs
+++ Assets/Script/GreenBird.cs
@@ -8,11 +8,13 @@ public class GreenBird : MonoBehaviour,
 {
     private SpriteRenderer spriteRenderer;
     private Camera mainCamera;
+    Vector3 _initialPosition;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         mainCamera = Camera.main;
+        _initialPosition = transform.position;
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -23,6 +25,11 @@ public class GreenBird : MonoBehaviour,
     public void OnPointerUp(PointerEventData eventData)
     {
         spriteRenderer.color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        //
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
     public void OnDrag(PointerEventData eventData)
